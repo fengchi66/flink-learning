1250a8cfe1c6ddfa573cea28dc68edaee7a673c1
wufc <wufengchi@yit.com>
2020-09-09 00:41:40 +0800

    add README.MD

---
+++ table/src/main/scala/com/yit/data/queries/Joins.scala
@@ -0,0 +1,9 @@
+package com.yit.data.queries
+
+/**
+ * created by wufc 
+ * on 2020/9/9
+ */
+object Joins {
+
+}
--- table/src/main/scala/com/yit/data/queries/Operations.scala
+++ table/src/main/scala/com/yit/data/queries/Operations.scala
@@ -9,6 +9,7 @@ import org.apache.flink.types.Row
 /**
  * created by wufc 
  * on 2020/9/8
+ * 对一条流进行查询
  */
 object Operations {
   def main(args: Array[String]): Unit = {
@@ -47,8 +48,67 @@ object Operations {
         |GROUP BY TUMBLE(rt, INTERVAL '5' SECOND), event
         |""".stripMargin)
 
+    /**
+     * 5. Over Window aggregation:
+     * 注意： 所有的聚合必须定义到同一个窗口中，即相同的分区、排序和区间。当前仅支持 PRECEDING (无界或有界) 到 CURRENT ROW
+     * 范围内的窗口、FOLLOWING 所描述的区间并未支持，ORDER BY 必须指定于单个的时间属性。
+     * 每来一条数据发出一次结果
+     */
+//    val table3 = tableEnv.sqlQuery(
+//      """
+//        |SELECT COUNT(1) OVER (
+//        |  PARTITION BY event
+//        |  ORDER BY rt
+//        |  ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
+//        |FROM ods_event_raw
+//        |
+//        |""".stripMargin)
+//
+//    tableEnv.sqlQuery(
+//      """
+//        |SELECT COUNT(amount) OVER w, SUM(amount) OVER w
+//        |FROM Orders
+//        |WINDOW w AS (
+//        |  PARTITION BY user
+//        |  ORDER BY proctime
+//        |  ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
+//        |""".stripMargin)
 
-      table2
+    /**
+     * 6. Distinct
+     * 对于流处理查询，根据不同字段的数量，计算查询结果所需的状态可能会无限增长
+     */
+    val table4 = tableEnv.sqlQuery("SELECT DISTINCT event FROM ods_event_raw")
+
+    /**
+     * 7. Grouping sets, Rollup, Cube
+     * 如果您经常需要对数据进行多维度聚合分析（例如既需要按照a列聚合，也要按照b列聚合，同时要按照a和b两列聚合），
+     * 您可以使用GROUPING SETS语句进行多维度聚合分析，避免多次使用UNION ALL影响性能。
+     */
+    val grouping_sql =
+      """
+        |SELECT
+        |    `month`,
+        |    `day`,
+        |    count(distinct `username`) as uv
+        |FROM tmall_item
+        |group by
+        |grouping sets((`month`),(`month`,`day`));
+        |""".stripMargin
+
+    /**
+     * 8. Having
+     */
+
+    val table5 = tableEnv.sqlQuery(
+      """
+        |SELECT event, COUNT(1)
+        |FROM ods_event_raw
+        |GROUP BY event
+        |HAVING COUNT(1) > 200
+        |""".stripMargin)
+
+    table5
       .toRetractStream[Row]
       .print()
 
